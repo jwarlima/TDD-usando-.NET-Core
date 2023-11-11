f4c7d24025faafa5d760e29a6c001b92a8dcff4e
jwarlima <jwar.lima@gmail.com>
2023-11-11 20:35:31 -0300

    Adicionar arquivos de projeto.

---
+++ NewTalents/Calculadora.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTalents
+{
+    public class Calculadora
+    {
+        private List<string> listaHistorico;
+        private string data;
+
+        public Calculadora(string data)
+        {
+            listaHistorico = new List<string>();
+            this.data = data;
+        }
+
+        public int Somar(int val1, int val2)
+        {
+            int res = val1 + val2;
+
+            listaHistorico.Insert(0, "Res: " + res + " - data: " + data);
+            
+            return res;
+        }
+
+        public int Subtrair(int val1, int val2)
+        {
+            int res = val1 - val2;
+
+            listaHistorico.Insert(0, "Res: " + res + " - data: " + data);
+
+            return res;
+        }
+
+        public int Multiplicar(int val1, int val2)
+        {
+            int res = val1 * val2;
+
+            listaHistorico.Insert(0, "Res: " + res + " - data: " + data);
+
+            return res;
+        }
+
+        public int Dividir(int val1, int val2)
+        {
+            int res = val1 / val2;
+
+            listaHistorico.Insert(0, "Res: " + res + " - data: " + data);
+
+            return res;
+        }
+
+        public List<String> Historico()
+        {
+            return listaHistorico.Take(3).ToList();
+        }
+    }
+}
